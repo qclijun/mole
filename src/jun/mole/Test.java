8ee4ef12bf0255d87b76b7bb748fe661edca45eb
qclijun <jun235@163.com>
2015-07-30 13:38:33 +0800

    logger added

--- src/jun/mole/Test.java
+++ src/jun/mole/Test.java
@@ -1,10 +1,18 @@
 package jun.mole;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Test {
+	private static final Logger logger = LoggerFactory.getLogger(Test.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		logger.debug("Enter main");
+
+		System.out.println("Test ant  sfasfa");
+
+		logger.debug("leave main");
 	}
 
 }
